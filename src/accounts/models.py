0b605a5e7da2af3627903ec7d344a3e5a0a7535d
Boilnardo <kazeminx@gmail.com>
2024-06-21 09:54:44 +0330

    version 6

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -44,6 +44,7 @@ class Order(models.Model):
   customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
   product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
   status = models.CharField(max_length=200, null=True, choices=STATUS)
+  note = models.CharField(max_length=1000, null=True)
   updated = models.DateTimeField(auto_now=True)
   created = models.DateTimeField(auto_now_add=True, null=True)
    
